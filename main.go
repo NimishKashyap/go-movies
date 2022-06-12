49fedbad8ca3898ce534e581197e0af033a4bc01
NimishKashyap <nimish.kashyap2020@vitstudent.ac.in>
2022-06-12 13:05:09 +0530

    main method done

--- main.go
+++ main.go
@@ -1,11 +1,39 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
 type Movie struct {
+	ID       string    `json:"id"`
+	ISBN     string    `json:"isbn"`
+	Title    string    `json:"title"`
+	Director *Director `json:"director"`
 }
 
 type Director struct {
+	Firstname string `json:"firstname"`
+	Lastname  string `json:"lastname"`
 }
 
-func main() {
+var movies []Movie
 
+func main() {
+	r := mux.NewRouter()
+
+	movies = append(movies, Movie{ID: "1", ISBN: "978-4-7741-8411-1", Title: "Get Go", Director: &Director{Firstname: "Nimish", Lastname: "Kashyap"}})
+	movies = append(movies, Movie{ID: "2", ISBN: "978-4-7741-8511-1", Title: "Get React", Director: &Director{Firstname: "Neelabh", Lastname: "Kashyap"}})
+
+	r.HandleFunc("/movies", getMovies).Methods("GET")
+	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
+	r.HandleFunc("/movies", createMovie).Methods("POST")
+	r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
+	r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
+
+	fmt.Println("Starting server on port 8080")
+	log.Fatal(http.ListenAndServe(":8080", r))
 }

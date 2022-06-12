1d3fe6f400bddc84be3efd4565dccc9199918f58
NimishKashyap <nimish.kashyap2020@vitstudent.ac.in>
2022-06-12 17:43:11 +0530

    added delete functionality

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -29,11 +30,31 @@ func main() {
 	movies = append(movies, Movie{ID: "2", ISBN: "978-4-7741-8511-1", Title: "Get React", Director: &Director{Firstname: "Neelabh", Lastname: "Kashyap"}})
 
 	r.HandleFunc("/movies", getMovies).Methods("GET")
-	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
-	r.HandleFunc("/movies", createMovie).Methods("POST")
-	r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
-	r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
+	// r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
+	// r.HandleFunc("/movies", createMovie).Methods("POST")
+	// r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
+	// r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
 
 	fmt.Println("Starting server on port 8080")
 	log.Fatal(http.ListenAndServe(":8080", r))
+}
+
+func getMovies(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Endpoint Hit: getMovies")
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(movies)
+}
+
+func deleteMovie(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Endpoint Hit: deleteMovie")
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range movies {
+		if item.ID == params["id"] {
+			movies = append(movies[:index], movies[index+1:]...)
+			break
+		}
+	}
+
+	json.NewEncoder(w).Encode("Movie with ID " + params["id"] + " deleted")
 }
